0bb9132cd37770c78e99e3bed145f753628ecb2e
mrshahriar <toshahriarshakil@gmail.com>
2023-02-17 06:21:51 +0600

    working on gatsby-bootcamp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
-import * as React from "react"
+import React from "react";
+import { Link, graphql, useStaticQuery } from "gatsby";
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+import Layout from "../components/layout";
+
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `);
+  return (
+    <Layout>
+      <h1>Hello.</h1>
+      <h2>
+        I'm {data.site.siteMetadata.author}, a full-stack developer living in
+        beautiful Bangladesh.
+      </h2>
+      <p>
+        Need a developer? <Link to="/contact">Contact me.</Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default IndexPage;
---
+++ src/posts/gatsby/gatsby.md
@@ -0,0 +1,13 @@
+---
+title: "The Great Gatsby Bootcamp"
+date: "2023-02-16"
+---
+
+I just launched a new bootcamp!
+![Grass Image](./grass.png)
+
+## Topics Covered
+
+1. Gatsby
+2. GraphQL
+3. React
